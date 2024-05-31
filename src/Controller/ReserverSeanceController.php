22ed6f8f740183283788ce28524667c7b7552005
bleau <lulubleau39@gmail.com>
2024-05-31 21:43:57 +0200

    pb token not found

--- src/Controller/ReserverSeanceController.php
+++ src/Controller/ReserverSeanceController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Repository\FilmRepository;
+use App\Repository\SalleRepository;
 use App\Repository\SeanceRepository;
 use App\Service\ReserverSeance;
 use App\Service\ReserverSeanceRequete;
 use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
@@ -23,7 +26,7 @@ class ReserverSeanceController extends AbstractController
         $email = $token->getUser()->getUserIdentifier();
         $donnees = json_decode($request->getContent(), true);
 
-        $requete = new ReserverSeanceRequete($donnees['seance_id'],$donnees['nombre_place_resa']);
+        $requete = new ReserverSeanceRequete($donnees['idSeance'],$donnees['nbPlace']);
 
         try
         {
@@ -40,13 +43,27 @@ class ReserverSeanceController extends AbstractController
         }
     }
 
-    #[Route('/info-seance', name: 'app_info_seance')]
-    public function infoSeance(\Symfony\Component\HttpFoundation\Request $request, ReserverSeance $reserverSeance, SerializerInterface $serializer, TokenInterface $tokenInterface): Response
+    #[Route('api/info-seance', name: 'app_info_seance')]
+    public function infoSeance(Request $request, ReserverSeance $reserverSeance, SerializerInterface $serializer, TokenInterface $tokenInterface, SeanceRepository $seanceRepository,FilmRepository $filmRepository, SalleRepository $salleRepository): Response
     {
+        $donnees = json_decode($request->getContent(), true);
+        $idSeance = $donnees["idSeance"];
+        if ($idSeance == null) {
+            // Si erreur on renvoie status 400 avec l'erreur
+            return new JsonResponse("L'ID de la seance n'a pas ete trouve", 400);
+        }
+
+        $seance = $seanceRepository->findOneBy(["id" => $idSeance]);
+        if ($seance == null) {
+            // Si erreur on renvoie status 400 avec l'erreur
+            return new JsonResponse("La seance n'a pas ete trouve", 400);
+        }
+
         // Si pas d'erreur on renvoie le User avec un status 201
-        $reservationSerialized = $serializer->serialize($reservation, 'json', ['groups' => 'info_reservation']);
-        return new Response($reservationSerialized, 201, [
+        $seanceSerialized = $serializer->serialize($seance, 'json', ['groups' => 'info_seance']);
+        return new Response($seanceSerialized, 201, [
             'content-type' => 'application/json'
         ]);
+
     }
 }
